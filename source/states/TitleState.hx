e13e83c4b6a405805d23b2a050881bab8a6df6c8
aflac <60161366+aflacc@users.noreply.github.com>
2024-09-16 13:17:22 -0700

    add more window names
    window names window names window names window names window names window names window names window names window names window names window names window names window names window names

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -36,10 +36,10 @@ class TitleState extends MusicBeatState
 		if (FlxG.random.bool(20)) // 20% chance to happen
 		{
 			var bald:Array<String> = [
-				"Bald", "Balmed", "Bold", "Balled", "Bowled", "Mald", "Called", "Balbed", "Bulb", "Pulp", "Beef", "Bah", "Burp"
+				"Bald", "Balmed", "Bold", "Balled", "Bowled", "Mald", "Called", "Balbed", "Bulb", "Pulp", "Beef", "Bah", "Burp", "Tall"
 			];
 			var gru:Array<String> = [
-				"Gru", "Grew", "Glue", "Grue", "Blue", "Stew", "Shoe", "Poo", "Brew", "Starch Jello", "Goo"
+				"Gru", "Grew", "Glue", "Grue", "Blue", "Stew", "Shoe", "Poo", "Brew", "Starch Jello", "Goo", "New", "Few"
 			];
 			var chosenBald = FlxG.random.getObject(bald);
 			var chosenGru = FlxG.random.getObject(gru);
